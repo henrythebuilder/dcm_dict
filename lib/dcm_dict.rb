a14d1d1d07daa0fc8bea227d32ee8b3cc7746b11
henrythebuilder <henrythebuilder@yahoo.it>
2014-07-21 17:38:52 +0200

    Renamed array and string refinement file to match their namespace

--- lib/dcm_dict.rb
+++ lib/dcm_dict.rb
@@ -17,8 +17,8 @@
 #  along with DcmDict.  If not, see <http://www.gnu.org/licenses/>.
 #
 require "dcm_dict/version"
-require "dcm_dict/refine/string_refine"
-require "dcm_dict/refine/array_refine"
+require "dcm_dict/refine/string_refine_internal"
+require "dcm_dict/refine/array_refine_internal"
 require "dcm_dict/xml/node_set"
 require "dcm_dict/encoder/data_to_code"
 require "dcm_dict/data_element_record"
