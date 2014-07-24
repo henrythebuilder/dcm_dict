df490c8da43608cb90ac0abb353d4d3e41337f2d
henrythebuilder <henrythebuilder@yahoo.it>
2014-07-24 16:15:20 +0200

    Add Symbol refinement for main access to Data Element Dictionary

--- spec/dcm_dict/refine/symbol_refine_spec.rb
+++ spec/dcm_dict/refine/symbol_refine_spec.rb
@@ -17,19 +17,18 @@
 #  along with DcmDict.  If not, see <http://www.gnu.org/licenses/>.
 #
 require 'spec_helper'
-describe "Array refinement" do
-  using DcmDict::Refine::ArrayRefine
+describe "Symbol refinement" do
+  using DcmDict::Refine::SymbolRefine
   describe "should permit access to single data element dictionary information" do
     describe "for standard tag" do
       [
-        '[0x0010,0x1005]', '[0x0028,0x1200]', '[0x0014,0x3050]'
-      ].each do |stag|
-        describe "#{stag}" do
-          tag = eval(stag)
+        :patient_birth_name, :gray_lookup_table_data, :dark_current_counts
+      ].each do |tag|
+        describe "#{tag}" do
           obj = DcmDict::Dictionary::TheDataElementDictionary.feature_of(tag)
           DcmDict::Dictionary::DataElementMethodMap.flatten.uniq.each do |method|
-            it "as #{stag}.#{method.to_s} > #{obj.send(method).inspect}" do
-              value = eval("#{stag}.#{method.to_s}")
+            it "as #{tag.inspect}.#{method.to_s} > #{obj.send(method).inspect}" do
+              value = eval("#{tag.inspect}.#{method.to_s}")
               expect(value).to eq(obj.send(method))
             end
           end
@@ -37,32 +36,10 @@ describe "Array refinement" do
       end
     end
 
-    describe "for multiple tag" do
-      {
-        '[0x6046,0x0040]' => { name: "Overlay Type",
-                           tag_multiple: true,
-                           tag_name: "Overlay Type",
-                           keyword: "OverlayType",
-                           tag_key: "OverlayType",
-                           vr: [:CS], tag_vr: [:CS], vm: ["1"], tag_vm: ["1"],
-                           tag: [0x6046, 0x0040], tag_ary: [0x6046, 0x0040],
-                           tag_ps: "(60XX,0040)",
-                           tag_sym: :overlay_type,
-                           tag_ndm: "60460040",
-                           tag_str: "(6046,0040)" }
-      }.each do |stag, data|
-        data.each do |key, exp_value|
-          it "as #{stag}.#{key} > #{exp_value.inspect}" do
-            value = eval("#{stag}.#{key}")
-            expect(value).to eq(exp_value)
-          end
-        end
-      end
-    end
   end
 
   it "should get single 'information record' for a data element with 'data_element' method" do
-    value = [0x0010,0x1005].data_element
+    value = :patient_birth_name.data_element
     expect(value).to be_a(DcmDict::Dictionary::DataElementRecord)
   end
 
