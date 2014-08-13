db31d83ad919cbbdb414961c97d47a2b4867bcb2
henrythebuilder <henrythebuilder@yahoo.it>
2014-08-13 16:40:22 +0200

    Code refactoring

--- lib/dcm_dict/xml/xml_tool.rb
+++ lib/dcm_dict/xml/xml_tool.rb
@@ -21,7 +21,7 @@
 #  It is the redistributor's or user's responsibility to comply with any
 #  applicable local, state, national or international regulations.
 #
-require_relative 'constant'
+#require_relative 'constant'
 require_relative 'rexml_tool'
 require_relative 'nokogiri_tool'
 
@@ -30,31 +30,18 @@ module DcmDict
 
     # Main tool to handle xml data for uid and data element
     module XmlTool
-      @@xml_tool = XML.nokogiri_enable? ? NokogiriTool : RexmlTool
+      class << self
+        private
+        def xml_tool
+          XML.nokogiri_enable? ? NokogiriTool : RexmlTool
+        end
 
-      def self.extract_data_element_field_from_xml_tr(xml_tr_string)
-        @@xml_tool.extract_data_element_field_from_xml_tr(xml_tr_string)
-      end
-
-      def self.extract_data_element_field_from_tr_set(trset)
-        @@xml_tool.extract_data_element_field_from_tr_set(trset)
-      end
-
-      def self.extract_uid_field_from_xml_tr(xml_tr_string)
-        @@xml_tool.extract_uid_field_from_xml_tr(xml_tr_string)
-      end
-
-      def self.extract_uid_field_from_tr_set(trset)
-        @@xml_tool.extract_uid_field_from_tr_set(trset)
-      end
-
-      def self.create_xml_doc(xml_string)
-        @@xml_tool.create_xml_doc(xml_string)
-      end
-
-      def self.each_tr_set(doc, xpath, &block)
-        @@xml_tool.each_tr_set(doc, xpath, &block)
+        def method_missing(*args, &block)
+          m = args.shift
+          xml_tool.send(m, *args, &block)
+        end
       end
     end
+
   end
 end
