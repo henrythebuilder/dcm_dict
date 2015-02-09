9e0018a139f0ce2b6a46d8d252d38bf24924d2dc
henrythebuilder <henrythebuilder@yahoo.it>
2015-02-09 10:37:29 +0100

    Update Version and Changelog data

--- lib/dcm_dict/version.rb
+++ lib/dcm_dict/version.rb
@@ -22,6 +22,6 @@
 #  applicable local, state, national or international regulations.
 #
 module DcmDict
-  VERSION = "0.3.0"
-  DICOM_DOC_VERSION = "2014c"
+  VERSION = "0.4.0"
+  DICOM_DOC_VERSION = "2015a"
 end
