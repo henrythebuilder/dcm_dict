501206c77736d5081bb4dd27fc0e66b57d289076
henrythebuilder <henrythebuilder@yahoo.it>
2016-04-13 15:50:05 +0200

    Update version to 0.8.0

--- lib/dcm_dict/version.rb
+++ lib/dcm_dict/version.rb
@@ -22,6 +22,6 @@
 #  applicable local, state, national or international regulations.
 #
 module DcmDict
-  VERSION = "0.7.0"
-  DICOM_DOC_VERSION = "2016a"
+  VERSION = "0.8.0"
+  DICOM_DOC_VERSION = "2016b"
 end
