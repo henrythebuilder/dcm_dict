695b44e6a9e747df09f45edab99a52a1d9234f54
henrythebuilder <henrythebuilder@yahoo.it>
2017-12-13 09:05:08 +0100

    Update to 2017e Base Standard

--- lib/dcm_dict/version.rb
+++ lib/dcm_dict/version.rb
@@ -22,6 +22,6 @@
 #  applicable local, state, national or international regulations.
 #
 module DcmDict
-  VERSION = "0.15.0"
-  DICOM_DOC_VERSION = "2017d"
+  VERSION = "0.16.0"
+  DICOM_DOC_VERSION = "2017e"
 end
