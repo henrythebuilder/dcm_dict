02436dba3526993e895135a634b45fa2dbca1c58
henrythebuilder <henrythebuilder@yahoo.it>
2018-07-16 11:48:18 +0200

    Update to 2018c Base Standard

--- lib/dcm_dict/version.rb
+++ lib/dcm_dict/version.rb
@@ -22,6 +22,6 @@
 #  applicable local, state, national or international regulations.
 #
 module DcmDict
-  VERSION = "0.18.1"
-  DICOM_DOC_VERSION = "2018b"
+  VERSION = "0.19.0"
+  DICOM_DOC_VERSION = "2018c"
 end
